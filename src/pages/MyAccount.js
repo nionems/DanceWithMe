489161ba0df2e4233496b14605e24b4aa37c6e14
lionel <nionems@icloud.com>
2022-11-17 23:31:36 +1100

    mylistaccountpagestarted

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useNavigate, Link } from "react-router-dom"
+import PhotoLio from '../lionel.JPG'
 
 export function MyAccount( props ) {
     const navigate = useNavigate()
@@ -30,12 +31,15 @@ export function MyAccount( props ) {
         
         
         <div className= "My Account">
-             <div className="col text-center">
+             <div className="container text-center">
 
           
 
             <h1 style={{ color: "white" }}>My Account</h1>
-
+            <div class="p-3 mb-2 bg-dark text-white">
+            <h1>Lionel Coevoet</h1>
+            <div class="col-2 "> <img className='logo' src={PhotoLio} /></div>
+            </div>
 
          </div>
         </div>
--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useNavigate, Link } from "react-router-dom"
+import Logo from '../WhiteLogo-01.png'
 
 export function MyList( props ) {
     const navigate = useNavigate()
@@ -14,14 +15,63 @@ export function MyList( props ) {
         
         <div className= "My List">
             
-             <div className="col text-center">
+            <div className="container text-center">
 
-          
-
-            <h1 style={{ color: "white" }}>My Favourite</h1>
+{/* <div class="row-1"> <img className='logo' src={PhotoLio} /></div> */}
 
 
-         </div>
+{/* <!-- Stack the columns on mobile by making one full-width and the other half-width --> */}
+<div className="row m-5 ">
+    <div className="col-md-8"></div>
+    <h1>My List of Favourite</h1>
+    
+</div>
+
+{/* <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop --> */}
+<div class="row m-5">
+   
+<div class="p-3 mb-2 bg-danger text-white"> Your list
+
+</div>
+    <div class="p-3 mb-2 bg-dark text-white">
+    <div class="col-6 "> <img className='logo' src={Logo} /></div>
+        
+    </div>
+    <div class="p-3 mb-2 bg-dark text-white">
+    <div class="col-6 "> <img className='logo' src={Logo} /></div>
+
+
+    </div>
+    <div class="p-3 mb-2 bg-dark text-white">
+    <div class="col-6 "> <img className='logo' src={Logo} /></div>
+    
+    </div>
+    <div class="p-3 mb-2 bg-dark text-dark">
+    <div class="col-6 "> <img className='logo' src={Logo} /></div>
+
+
+    </div>
+    <div class="p-3 mb-2 bg-dark text-white">
+    <div class="col-6 "> <img className='logo' src={Logo} /></div>
+    dhfjkdhfkdjhfkdjsfhkfhjksd
+    jdjdhcshcidjcidjcicjdijdijc
+    kchjkdchlchjkdchjlchildcihc
+    
+    </div>
+    <div class="col-6 col-md-4-bg-dark class">
+  
+
+    </div>
+    {/* <div class="col-6 col-md-4"><img className='logo' src={Logo} /></div> */}
+
+</div>
+
+{/* <!-- Columns are always 50% wide, on mobile and desktop --> */}
+<div class="row m-5">
+    
+</div>
+
+</div>
         </div>
 
 
